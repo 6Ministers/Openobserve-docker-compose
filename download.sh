52f4edeaa035a639a2392897fcccf2b5afe54615
6ministers <manyatovskiy@gmail.com>
2025-03-29 14:19:14 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 DESTINATION=$1
 
-# clone Flectra directory
+# clone Openobserve directory
 git clone --depth=1 https://github.com/6Ministers/Openobserve-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 
